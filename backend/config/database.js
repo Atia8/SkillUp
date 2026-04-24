1590baeae4365db36f32e5f52258a1f109e31dbe
Atia Zaman <141864071+Atia8@users.noreply.github.com>
2026-04-24 12:27:47 +0400

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -36,7 +36,8 @@ if (process.env.MYSQL_URL) {
     user: process.env.MYSQLUSER ||process.env.DB_USER || 'root', 
     password: process.env.MYSQLPASSWORD ||process.env.DB_PASSWORD|| '',
     database: process.env.MYSQLDATABASE || process.env.DB_NAME||'skillshare',
-    port: process.env.MYSQLPORT || 3306,
+    port: process.env.MYSQLPORT ||process.env.DB_PORT|| 3306,
+     ssl: { rejectUnauthorized: false },
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
